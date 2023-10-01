a366919b35e5c801961585431ef68b5388d5b742
HostCase <430ap430@gmail.com>
2023-10-01 16:48:29 +0300

    added float #need add modules

--- projectLETI1.cpp
+++ projectLETI1.cpp
@@ -8,25 +8,35 @@ int main()
     setlocale(0, "");
 
     cout <<"Размер в байтах у различных переменных:"<<"\n" << "int" << " " << sizeof(int) << "\n" << "short int" << " " << sizeof(short int) << "\n" << "long int" << " " << sizeof(long int) << "\n" << "float" << " " << sizeof(float) << "\n" << "double" << " " << sizeof(double) << "\n" << "long double" << " " << sizeof(long double) << "\n";
- 
-
-    /////////
-    /////////
-
-
+    
+    /// 
     int inputnumber, choosetype;
     cout << "Введите число" << "\n";
     cin >> inputnumber;
-    cout << "Выберите тип числа(введите 1 для int, 2 для shortint)";
+    cout << "Выберите тип числа(введите 1 для int, 2 для shortint)"<<"\n";
     int order = sizeof(int) * 8 - 1;
         int mask = 1 << order;
-    for (int i = 0; i <= order; i++) {
-        cout << ((inputnumber & mask) ? 1 : 0);
-        inputnumber = inputnumber << 1;
-        if (!i or (i + 1) % 8 == 0) {
-        cout << "";
-    }
-}
+        for (int i = 0; i <= order; i++) {
+            cout << ((inputnumber & mask) ? 1 : 0);
+            inputnumber = inputnumber << 1;
+            if (i == 0 or (i + 1) % 8 == 0) {
+                cout << " ";
+            }
+        }
+    ////float////
+    union {
+        int int2bitfloat;
+        float floatin = 3.14;
+    };
 
+    cout <<"\n"<<"float \n";
+    /// add new mask for float
+    for (int i = 0; i <= order; i++) {
+        cout << ((int2bitfloat & mask) ? 1 : 0);
+        int2bitfloat = int2bitfloat << 1;
+        if (i == 0 or (i + 1) % 8 == 0) {
+            cout << " ";
+        }
+    }
     return 0;
 }
