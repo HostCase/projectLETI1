5cf37f433f8031b5b0b79815e3b78eb3738b0ced
HostCase <430ap430@gmail.com>
2023-10-01 15:09:05 +0300

    second part

--- projectLETI1.cpp
+++ projectLETI1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    setlocale(0,"");
+    setlocale(0, "");
     int fake;
     fake = 1;
     short int shorta;
@@ -25,8 +25,17 @@ int main()
 
 
     int inputnumber, choosetype;
-    cout << "Введите число"<<"\n";
+    cout << "Введите число" << "\n";
     cin >> inputnumber;
     cout << "Выберите тип числа(введите 1 для int, 2 для shortint)";
+    int order = sizeof(fake) * 8 - 1
+        int mask = 1 << order;
+    for (int i = 0; i <= order; i++){
+        cout << ((a & mask) ? 1 : 0);
+    a = a << 1;
+    if (!i or (i + 1) % 8 == 0)
+        cout << "";
+}
+
     return 0;
 }
