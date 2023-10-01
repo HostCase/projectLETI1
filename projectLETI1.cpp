95e3f49cc8986df55b6cfdb49bfbe15a23d31ffb
HostCase <430ap430@gmail.com>
2023-10-01 13:54:00 +0300

    Добавьте файлы проекта.

---
+++ projectLETI1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int fake;
+    fake = 1;
+    short int shorta;
+    shorta = fake;
+    long int longa;
+    longa = fake;
+    float floata;
+    floata = fake;
+    double doublea;
+    doublea = fake;
+    long double longdoublea;
+    longdoublea = fake;
+    cout << sizeof(fake) << "\n" << sizeof(short int) << "\n" << sizeof(long int) << "\n" << sizeof(float) << "\n" << sizeof(double) << "\n" << sizeof(long double) << "\n";
+    return 0;
+}
