fb556de4cefd0e2f0d7f954af12c111b071420b7
HostCase <430ap430@gmail.com>
2023-10-03 20:55:34 +0300

    complited idz3

--- projectLETI1.cpp
+++ projectLETI1.cpp
@@ -6,21 +6,35 @@ using namespace std;
 int main()
 {
     setlocale(0, "");
-    int choosetype;
-    cout << "Выберите номер задания\n1 Вывести задание номер один \n2 Вывести задание номер два\n3 Вывести задание номер три\n4 Вывести задание номер четыре ";
+    int choosetype, inversionbit;
+    cout << "Выберите номер задания\n1 Вывести задание номер один(бит буйт) \n2 Вывести задание номер два(int)\n3 Вывести задание номер три(float)\n4 Вывести задание номер четыре(его нет)\n";
     cin >> choosetype;
 
     if (choosetype == 1) {
-        cout << "Размер в байтах у различных переменных:" << "\n" << "int" << " " << sizeof(int) << "\n" << "short int" << " " << sizeof(short int) << "\n" << "long int" << " " << sizeof(long int) << "\n" << "float" << " " << sizeof(float) << "\n" << "double" << " " << sizeof(double) << "\n" << "long double" << " " << sizeof(long double) << "\n";
+        cout << "Размер в байтах у различных переменных:" << "\n";
+        cout << "int" << " " << sizeof(int) << "\n";
+        cout << "short int" << " " << sizeof(short int) << "\n";
+        cout << "long int" << " " << sizeof(long int) << "\n";
+        cout << "float" << " " << sizeof(float) << "\n";
+        cout << "double" << " " << sizeof(double) << "\n";
+        cout << "long double" << " " << sizeof(long double) << "\n";
     }
     else if (choosetype == 2) {
         int inputnumber;
         cout << "Введите число" << "\n";
         cin >> inputnumber;
+        cout << "Сколько раз инвертировать число?\n";
+        cin >> inversionbit;
         int order = sizeof(int) * 8 - 1;
         unsigned int mask = 1 << order;
         for (int i = 0; i <= order; i++) {
             cout << ((inputnumber & mask) ? 1 : 0);
+            if (inversionbit == 0 or inversionbit % 2 == 0) {
+                cout << ((inputnumber & mask) ? 1 : 0);
+            }
+            else {
+                cout << ((inputnumber & mask) ? 0 : 1);
+            }
             mask = mask >> 1;
             if (i == 0 or (i + 1) % 8 == 0) {
                 cout << " ";
@@ -44,11 +58,18 @@ int main()
         cout << "\n" << "Введите значение для типа float(используйте . для отделения целой части) \n";
         MyUnion g;
         cin >> g.floatinTObit;
+        cout << "Сколько раз инвертировать число?\n";
+        cin >> inversionbit;
         cout << "\n" << "float \n";
         int orderf = sizeof(float) * 8 - 1;
         unsigned int maskf = 1 << orderf;
         for (int i = 0; i <= orderf; i++) {
-            cout << ((g.int2bitfloat & maskf) ? 1 : 0);
+            if (inversionbit == 0 or inversionbit % 2 == 0) {
+                cout << ((g.int2bitfloat & maskf) ? 1 : 0);
+            }
+            else{
+                cout << ((g.int2bitfloat & maskf) ? 0 : 1);
+            }
             maskf = maskf >> 1;
             if (i == 0 or i == 8) {
                 cout << " ";
@@ -58,7 +79,7 @@ int main()
 
 
     else if (choosetype = 4) {
-        cout << "\nтут что-то будет)xd";
+        cout << "\nпочему...";
 
 
 
